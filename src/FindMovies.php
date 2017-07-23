9788b6ffc069aa20993c83d3fd69d87b9a7d676c
96qbhy <96qbhy@gmail.com>
2017-07-23 18:35:07 +0800

    添加版本信息，吃饭

--- src/FindMovies.php
+++ src/FindMovies.php
@@ -4,27 +4,45 @@ namespace Vbot\FindMovies;
 
 use Hanson\Vbot\Extension\AbstractMessageHandler;
 use Illuminate\Support\Collection;
+use Qbhy\FindMovies\Finder;
 
 class FindMovies extends AbstractMessageHandler
 {
+
+    public $author = '96qbhy';
+
+    public $name = 'find-movies';
+
+    public $zhName = '找电影';
+
+    public $version = '1.0';
 
     /**
      * 注册拓展时的操作.
      */
     public function register()
     {
-        // TODO: Implement register() method.
+        /**
+         * 初始化 Finder
+         */
+        Finder::init();
     }
 
     /**
      * 开发者需要实现的方法.
      *
-     * @param Collection $collection
-     *
+     * @param Collection $message
      * @return mixed
      */
     public function handler(Collection $message)
     {
+        $content = $message['content'];
+        if ($message['type'] === 'text' and strpos($content, '找电影 ') === 0 and strlen($content) > 4) {
+            $keyword = str_replace("找电影 ", '', $content);
 
+            $results = Finder::find($keyword, 5);
+
+        }
+        return null;
     }
 }
